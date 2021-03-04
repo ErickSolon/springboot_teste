402560207065f7c58d7810fe150e5dac80679053
root <root@localhost.localdomain>
2021-03-04 17:22:50 -0300

    Commit.

---
+++ src/main/java/com/erick/projetos/conf/ConfiguracaoDoSite.java
@@ -0,0 +1,12 @@
+package com.erick.projetos.conf;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConfiguracaoDoSite {
+
+	public String index() {
+		return "index";
+	}
+	
+}
